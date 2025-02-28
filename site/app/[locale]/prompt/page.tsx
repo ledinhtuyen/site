df1b22412c0a7522668859e93fae92e72d302515
ledinhtuyen <tuyenldjp@gmail.com>
2025-02-28 13:29:08 +0900

    add table

--- site/app/[locale]/prompt/page.tsx
+++ site/app/[locale]/prompt/page.tsx
@@ -1,70 +1,55 @@
-import { ContentLayout } from "@/components/panel/content-layout";
-import { Note } from "@/components/ui/note";
-// import * as React from "react";
+import type { SearchParams } from "@/types";
+import * as React from "react";
 
-// import { DataTableSkeleton } from "@/components/data-table/data-table-skeleton";
-// // import { DateRangePicker } from "@/components/date-range-picker";
-// // import { Shell } from "@/components/shell";
-// import { Skeleton } from "@/components/ui/skeleton";
-// import { getValidFilters } from "@/lib/data-table/data-table";
+import { DataTableSkeleton } from "@/components/data-table/data-table-skeleton";
+import { Shell } from "@/components/shell";
+import { getValidFilters } from "@/lib/data-table";
 
-// import { FeatureFlagsProvider } from "./_components/feature-flags-provider";
-// import { TasksTable } from "./_components/tasks-table";
-// import {
-//   getTaskPriorityCounts,
-//   getTaskStatusCounts,
-//   getTasks,
-// } from "./_lib/queries";
-// import { searchParamsCache } from "./_lib/validations";
+import { FeatureFlagsProvider } from "./_components/feature-flags-provider";
+import { TasksTable } from "./_components/tasks-table";
+import {
+  getTaskPriorityCounts,
+  getTaskStatusCounts,
+  getTasks,
+} from "./_lib/queries";
+import { searchParamsCache } from "./_lib/validations";
 
-export default async function PromptsPage() {
-  // const searchParams = await props.searchParams;
-  // const search = searchParamsCache.parse(searchParams);
+interface IndexPageProps {
+  searchParams: Promise<SearchParams>;
+}
 
-  // const validFilters = getValidFilters(search.filters);
+export default async function IndexPage(props: IndexPageProps) {
+  const searchParams = await props.searchParams;
+  const search = searchParamsCache.parse(searchParams);
 
-  // const promises = Promise.all([
-  //   getTasks({
-  //     ...search,
-  //     filters: validFilters,
-  //   }),
-  //   getTaskStatusCounts(),
-  //   getTaskPriorityCounts(),
-  // ]);
+  const validFilters = getValidFilters(search.filters);
+
+  const promises = Promise.all([
+    getTasks({
+      ...search,
+      filters: validFilters,
+    }),
+    getTaskStatusCounts(),
+    getTaskPriorityCounts(),
+  ]);
 
   return (
-    // <Shell className="gap-2">
-      // <FeatureFlagsProvider>
-      //   <React.Suspense fallback={<Skeleton className="h-7 w-52" />}>
-      //     {/* <DateRangePicker
-      //       triggerSize="sm"
-      //       triggerClassName="ml-auto w-56 sm:w-60"
-      //       align="end"
-      //       shallow={false}
-      //     /> */}
-      //   </React.Suspense>
-      //   <React.Suspense
-      //     fallback={
-      //       <DataTableSkeleton
-      //         columnCount={6}
-      //         searchableColumnCount={1}
-      //         filterableColumnCount={2}
-      //         cellWidths={["10rem", "40rem", "12rem", "12rem", "8rem", "8rem"]}
-      //         shrinkZero
-      //       />
-      //     }
-      //   >
-      //     <TasksTable promises={promises} />
-      //   </React.Suspense>
-      // </FeatureFlagsProvider>
-    // </Shell>
-    <ContentLayout title={"Prompts"}>
-      <Note>
-        <p>
-          This is a prompt page. It is a placeholder for a page that will be
-          created in the future.
-        </p>
-      </Note>
-    </ContentLayout>
+    <Shell className="gap-2">
+      <FeatureFlagsProvider>
+        <React.Suspense
+          fallback={
+            <DataTableSkeleton
+              columnCount={6}
+              searchableColumnCount={1}
+              filterableColumnCount={2}
+              cellWidths={["10rem", "40rem", "12rem", "12rem", "8rem", "8rem"]}
+              shrinkZero
+            />
+          }
+        >
+          <TasksTable promises={promises} />
+        </React.Suspense>
+      </FeatureFlagsProvider>
+    </Shell>
   );
 }
